9d780a2e894d388d8b1dbfdb3769e7ebfd8593be
Yazan Homssi <121850764+Shinoruba@users.noreply.github.com>
2023-11-27 20:32:02 +0800

    Javadocs for Area3

--- src/Area3.java
+++ src/Area3.java
@@ -1,22 +1,42 @@
+/**
+ *  The Area3 class handles choosing the appropriate EL creatures to be shown in the third area, 
+ *  which in this case are ALL creatures.
+ * 
+ *  @author Shinoruba
+ *  @author JSTP8330
+ *  @version 1
+ */
 public class Area3 extends Area {
 
+    /**
+     * Constructs an Area3 object, initializing it with a specific size (4x4).
+     */
     public Area3() {
         super(4, 4);
     }
 
+    /**
+     * Displays the layout of Area 3, marking the player's position with "P".
+     * Overrides the method in the superclass (Area).
+     */
     @Override
     public void displayArea() {
         System.out.println("Area 3:");
         super.displayArea();
     }
 
-
+    /**
+     * Handles the encounter of creatures in Area 3 based on their Evolution Level (EL).
+     * Overrides the method in the superclass (Area).
+     * 
+     * @return The encountered creature, allowing EL1, EL2, and EL3 creatures.
+     */
     @Override
     public Creature encounteredCreature() {
-        System.out.println("You encountered a creature in Area 2:");
-        
-        // Adjust the logic to only allow EL1 and EL2 and EL3 creatures
-        int el = random.nextInt(3) + 1; // Randomly choose between EL1 and EL2 and EL3
+        System.out.println("You encountered a creature in Area 3:");
+
+        // Adjust the logic to allow EL1, EL2, and EL3 creatures
+        int el = random.nextInt(3) + 1; // Randomly choose between EL1, EL2, and EL3
 
         switch (el) {
             case 1:
