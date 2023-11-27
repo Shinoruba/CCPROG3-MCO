786b8c32584083de5d91798aa654ab4a090f5d17
Yazan Homssi <121850764+Shinoruba@users.noreply.github.com>
2023-11-28 01:52:36 +0800

    Testing Constructor to work with GUI

--- src/Area2.java
+++ src/Area2.java
@@ -1,52 +1,50 @@
 /**
- *  The Area3 class handles choosing the appropriate EL creatures to be shown in the third area, 
- *  which in this case are ALL creatures.
+ *  The Area2 class handles choosing the appropriate EL creatures to be shown in the second area, 
+ *  which in this case are only EL1 & EL2 creatures.
  * 
  *  @author Shinoruba
  *  @author JSTP8330
  *  @version 1
  */
-public class Area3 extends Area {
+public class Area2 extends Area {
 
     /**
-     * Constructs an Area3 object, initializing it with a specific size (4x4).
+     * Constructs an Area2 object, initializing it with a specific size (3x3).
      * @param j
      * @param i
      */
-    public Area3(int i, int j) {
-        super(4, 4);
+    public Area2(int i, int j) {
+        super(3, 3);
     }
 
     /**
-     * Displays the layout of Area 3, marking the player's position with "P".
+     * Displays the layout of Area 2, marking the player's position with "P".
      * Overrides the method in the superclass (Area).
      */
     @Override
     public void displayArea() {
-        System.out.println("Area 3:");
+        System.out.println("Area 2:");
         super.displayArea();
     }
 
     /**
-     * Handles the encounter of creatures in Area 3 based on their Evolution Level (EL).
+     * Handles the encounter of creatures in Area 2 based on their Evolution Level (EL).
      * Overrides the method in the superclass (Area).
      * 
-     * @return The encountered creature, allowing EL1, EL2, and EL3 creatures.
+     * @return The encountered creature, limited to EL1 and EL2 creatures.
      */
     @Override
     public Creature encounteredCreature() {
-        System.out.println("You encountered a creature in Area 3:");
+        System.out.println("You encountered a creature in Area 2:");
 
-        // Adjust the logic to allow EL1, EL2, and EL3 creatures
-        int el = random.nextInt(3) + 1; // Randomly choose between EL1, EL2, and EL3
+        // Adjust the logic to only allow EL1 and EL2 creatures
+        int el = random.nextInt(2) + 1; // Randomly choose between EL1 and EL2
 
         switch (el) {
             case 1:
                 return encounterCreatureEL1();
             case 2:
                 return encounterCreatureEL2();
-            case 3:
-                return encounterCreatureEL3();
             default:
                 throw new IllegalStateException("Unexpected value: " + el);
         }
